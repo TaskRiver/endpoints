89617dc25bcbfc116a5247d197521350383390ec
jehfkemsy <jehf.denezaire@gmail.com>
2020-10-10 21:28:02 -0400

    feat: Added error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import Koa from "koa";
 import bodyParser from "koa-body";
 import cors from "@koa/cors";
 import routes from "./routes";
+import errorHandler from "./middlewares/error-handler";
 
 const app = new Koa();
 
-app.use(cors()).use(bodyParser()).use(routes());
+app.use(cors()).use(bodyParser()).use(errorHandler()).use(routes());
 
 app.onerror = e => {
   console.error(e);
--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,7 +1,21 @@
 import { Context, Next } from "koa";
+import { Middleware } from "koa-compose";
 
 /**
  * @param context
  * @param next
  */
-export default async function errorHandler(context: Context, next: Next) {}
+export default function errorHandler(): Middleware<Context> {
+  return async (ctx: Context, next: Next) => {
+    try {
+      await next();
+    } catch (error) {
+      ctx.status = (error.status as number) || 500;
+      ctx.body = {
+        error: true,
+        message: error?.message || "Something went wrong."
+      };
+      ctx.app.emit("error", error, ctx);
+    }
+  };
+}
